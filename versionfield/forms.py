072751c3cb008c6f206fdc3122166ad36dafc233
Egor Yurtaev <yurtaev.egor@gmail.com>
2014-10-14 18:33:19 +0700

    reformat code

--- versionfield/forms.py
+++ versionfield/forms.py
@@ -3,19 +3,20 @@ from version import Version
 from constants import DEFAULT_NUMBER_BITS
 from utils import convert_version_string_to_int
 
+
 class VersionField(forms.IntegerField):
-	def __init__(self,number_bits=DEFAULT_NUMBER_BITS,**kwargs):
-		self.number_bits = number_bits
-		return super(VersionField, self).__init__(**kwargs)
+    def __init__(self, number_bits=DEFAULT_NUMBER_BITS, **kwargs):
+        self.number_bits = number_bits
+        return super(VersionField, self).__init__(**kwargs)
 
-	def to_python(self,value):
-		"""
-		Verifies that value can be converted to a Version object
-		"""
-		if not value:
-			return None
+    def to_python(self, value):
+        """
+        Verifies that value can be converted to a Version object
+        """
+        if not value:
+            return None
 
-		if isinstance(value,basestring):
-			return Version(value,self.number_bits)		
+        if isinstance(value, basestring):
+            return Version(value, self.number_bits)
 
-		return Version(convert_version_int_to_string(value,self.number_bits),self.number_bits)
+        return Version(convert_version_int_to_string(value, self.number_bits), self.number_bits)
