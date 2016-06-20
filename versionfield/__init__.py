8a8e7d119f673293ed4ffd209242c70b66891a23
Egor Yurtaev <yurtaev.egor@gmail.com>
2016-06-20 20:31:54 +0600

    Django 1.10 compatibility #1

--- versionfield/__init__.py
+++ versionfield/__init__.py
@@ -2,15 +2,45 @@ from builtins import str
 from past.builtins import basestring
 from future.utils import python_2_unicode_compatible
 
+import django
 from django.db import models
-from django.utils.six import with_metaclass
 from .constants import DEFAULT_NUMBER_BITS
 from .version import Version
 from .utils import convert_version_string_to_int, convert_version_int_to_string
 from . import forms
 
+if django.VERSION[:2] <= (1, 7):
+    from django.utils.six import with_metaclass
 
-class VersionField(with_metaclass(models.SubfieldBase, models.BigIntegerField)):
+
+    class BaseField(with_metaclass(models.SubfieldBase, models.BigIntegerField)):
+        def to_python(self, value):
+            if isinstance(value, Version):
+                return int(value)
+
+            if isinstance(value, basestring):
+                return Version(value, self.number_bits)
+
+            if value is None:
+                return None
+
+            return Version(convert_version_int_to_string(value, self.number_bits), self.number_bits)
+else:
+    class BaseField(models.BigIntegerField):
+        def from_db_value(self, value, *args, **kwargs):
+            if isinstance(value, Version):
+                return int(value)
+
+            if isinstance(value, basestring):
+                return Version(value, self.number_bits)
+
+            if value is None:
+                return None
+
+            return Version(convert_version_int_to_string(value, self.number_bits), self.number_bits)
+
+
+class VersionField(BaseField):
     """
     A Field where version numbers are input/output as strings (e.g. 3.0.1)
     but stored in the db as converted integers for fast indexing
@@ -20,18 +50,6 @@ class VersionField(with_metaclass(models.SubfieldBase, models.BigIntegerField)):
     def __init__(self, number_bits=DEFAULT_NUMBER_BITS, *args, **kwargs):
         self.number_bits = number_bits
         super(VersionField, self).__init__(*args, **kwargs)
-
-    def to_python(self, value):
-        if isinstance(value, Version):
-            return int(value)
-
-        if isinstance(value, basestring):
-            return Version(value, self.number_bits)
-
-        if value is None:
-            return None
-
-        return Version(convert_version_int_to_string(value, self.number_bits), self.number_bits)
 
     def get_prep_value(self, value):
         if isinstance(value, basestring):
